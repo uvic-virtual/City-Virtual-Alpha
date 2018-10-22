a0bdd3d3b73e794198dbe47b5bd07d9f2a10bda7
Wade King <wkingnumber2@gmail.com>
2018-10-21 22:19:58 -0700

    Bug Fixes
    players are now able to each move their cameras independantly

--- Assets/myscripts/Player_network.cs
+++ Assets/myscripts/Player_network.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class Player_network : NetworkBehaviour {
-
-    public GameObject firstPersonCharacter;
-    public GameObject[] characterModel;
-
-    public override void OnStartLocalPlayer()
+namespace CityVirtual
+{
+    public class Player_Network : NetworkBehaviour
     {
-        GetComponent<FirstPersonController>().enabled = true;
-        firstPersonCharacter.SetActive(true);
+        public GameObject firstPersonCharacter;
+        public GameObject[] characterModel;
 
-        foreach(GameObject go in characterModel)
+        public override void OnStartLocalPlayer()
         {
-            go.SetActive(false);
+            GetComponent<FirstPersonController>().enabled = true;
+            firstPersonCharacter.SetActive(true);
+
+            foreach(GameObject go in characterModel)
+            {
+                go.SetActive(false);
+            }
         }
+
+
     }
 }
